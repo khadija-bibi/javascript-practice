edc8b4a07df46f6442f133429a917215fdd8bba1
khadija <sub99way1122@gmail.com>
2024-03-11 18:58:01 +0500

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId =144553
+let accountEmail = "k1122@gmail.com"
+var accountPassword ="12345"//not in use
+accountCity = "Jaipur"//don't use
+let accountState;
+
+//accountId = 2//not allowed
+accountEmail = "hc@gma.com"
+accountPassword = "2135545"
+accountCity = "jkwcjkeb"
+console.log(accountPassword);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat JS code as newer version
+//alert(3 + 3) on browser
+console.log(3+3)
+console.log("khadija")
+
+
+let name = "khadija"
+let age = 18
+let isLoggedIn = false
+
+/* number
+bigint
+string
+boolean
+null
+undefined
+symbol*/
+
+/* 
+-object-
+*/
+console.log(typeof "khadija")
+console.log(typeof null) //object
+console.log(typeof undefined)//undefined
